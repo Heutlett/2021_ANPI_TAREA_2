ce108214de9dd536640d73024dfd161a9589a57c
sebas-mora28 <semasmora@gmail.com>
2021-10-08 14:40:52 -0600

    Documentacion interna

--- Parte2/parte2_p2.py
+++ Parte2/parte2_p2.py
@@ -4,16 +4,40 @@ import sympy as sp
 import copy
 import matplotlib.pylab as plt 
 
+
+# Funcion que convierte en simbolicas las variables a utilizar  
+#
+# Sintaxis:  variables_simbolicas(variables)
+#
+# Parametros iniciales: 
+#            variables = array que contiene las variables de las funciones 
+#
+# Parametros de salida:                           
+#            v_var = Array con las varaibles convertidas en simbolicas 
+
 def variables_simbolicas(variables):
+
     n=len(variables)
     tam=np.arange(n)
     v_var=[]
+
     for i in tam:
         v_var.append(sp.Symbol(variables[i]))
+
     return v_var
 
 
-def funciones_simbolicas(f, x_sym):
+# Funcion que convierte las funciones a utilizar en simbolicas 
+#
+# Sintaxis:  funciones_simbolicas(f)
+#
+# Parametros iniciales: 
+#            f = array que contiene las funciones a utilizar  
+#
+# Parametros de salida:                           
+#            f_var = Array con las funciones convertidas en simbolicas 
+
+def funciones_simbolicas(f):
     n=len(f)
     tam=np.arange(0,n,1)
     f_var=[]
@@ -22,15 +46,40 @@ def funciones_simbolicas(f, x_sym):
     return f_var
 
 
+# Funcion calcula las derivadas parciales dado un array con funciones,
+# Se utiliza para como parte del calculo del Jacobiano
+#
+# Sintaxis:  derivadas_parciales(f, var)
+#
+# Parametros iniciales: 
+#            f = array que contiene las funciones a utilizar 
+#            v_var = array que contiene las variables simbolicas a utilizar 
+#
+# Parametros de salida:                           
+#            g = Array con las derivadas parciales de las funciones dadas
 
 def derivadas_parciales(f,v_var):
     n=len(v_var)
     g=[]
+
     for i in np.arange(0,n,1):
         g.append(sp.diff(f,v_var[i]))
 
     return g
 
+
+
+# Funcion que evalua el Jacobiano dado un conjunto de valores
+#
+# Sintaxis:  evaluar_jacobiano(f)
+#
+# Parametros iniciales: 
+#            J = Jacobiano
+#            x_sym = array que contiene las variables simbolicas 
+#            x = array que contiene los valores numericos  
+#
+# Parametros de salida:                           
+#            array con las funciones convertidas en simbolicas 
 
 def evaluar_jacobiano(J, x_sym, xk):
 
@@ -48,6 +97,17 @@ def evaluar_jacobiano(J, x_sym, xk):
     return np.array(J_k).astype(np.float64)
 
 
+# Funcion que evalua un array de funciones dado un conjunto de valores
+#
+# Sintaxis:  evaluar_jacobiano(f)
+#
+# Parametros iniciales: 
+#            f = array que contiene las funciones a utilizar
+#            x_sym = array que contiene las variables simbolicas 
+#            x = array que contiene los valores numericos  
+#
+# Parametros de salida:                           
+#             array con las funciones convertidas en simbolicas
 def evaluar_funciones(f, x_sym, xk):
 
     f_k = f.copy()
@@ -62,21 +122,45 @@ def evaluar_funciones(f, x_sym, xk):
 
 
 
-def jacobiano(f, x):
+
+# Funcion que calcula la matriz Jacobiana dado un conjunto de funciones
+#
+# Sintaxis:  jacobiano(f, x)
+#
+# Parametros iniciales: 
+#            f = array que contiene las funciones a utilizar 
+#            x_sym = array que contiene las variables simbolicas 
+#
+# Parametros de salida:                           
+#            array con las funciones convertidas en simbolicas
+
+def jacobiano(f, x_sym):
     
     m = len(f)
     J = []
 
     for i in range(m):
-        z = derivadas_parciales(f[i], x)
+        z = derivadas_parciales(f[i], x_sym)
         J.append(z)
 
     return np.array(J)
 
-#f = ["x^2 + y^2 + z^2 - 1", "2*x^2 + y^2 - 4*z", "3*x^2 - 4*y + z^2"]
-#x = ['x', 'y', 'z']
 
 
+# Variacion metodo de newton_raphson para resolver un sistemas de 
+# ecuaciones lineales
+#
+# Sintaxis:  newton_raphson(x0, f, x, tol, iterMax)
+#
+# Parametros iniciales: 
+#            x0 = vector con los valores iniciales
+#            f = array que contiene las funciones a utilizar 
+#            x = array que contiene las variables a utilizar
+#            tol = tolerancia 
+#            iterMax = numero de iteraciones maximas 
+#
+# Parametros de salida:                           
+#            xk = vector los valores
 def newton_raphson(x0, f, x, tol, iterMax):
 
 
@@ -86,9 +170,7 @@ def newton_raphson(x0, f, x, tol, iterMax):
 
     x_sym = variables_simbolicas(x)
     J = jacobiano(f,x_sym)
-    f_n = funciones_simbolicas(f, x_sym)
-
-
+    f_n = funciones_simbolicas(f)
 
 
     while(k < iterMax):
@@ -98,8 +180,6 @@ def newton_raphson(x0, f, x, tol, iterMax):
         J_k = evaluar_jacobiano(J, x_sym, xk).astype(np.float64)
 
         y = np.linalg.solve(J_k, f_k)
-
-
         xk = xk - y
 
         f_k = evaluar_funciones(f_n, x_sym, xk)
@@ -114,7 +194,8 @@ def newton_raphson(x0, f, x, tol, iterMax):
 
         
     print("La solucion del sistema de ecuaciones esta dado por el vector x = ", xk)
-            #Graficacion       
+    
+    #Graficacion       
     plt.rcParams.update({'font.size': 14})
     ejex=np.arange(1,k+1,1)
     fig, graf=plt.subplots()
@@ -126,15 +207,17 @@ def newton_raphson(x0, f, x, tol, iterMax):
     plt.show()
 
     
-    return xk
+    return xk, k, e_k, 
     
 
 
+
+#Fuciones a ejemplo obtenidas a partir del pdf
 f = ["x1^2 - 2*x1 - x2 + 0.5", "x1^2 + 4*x2^2 - 4"]
-x = ['x1', 'x2']
-x0 = [3,2]
-tol = 10**-1
-iterMax = 1000
+x = ['x1', 'x2'] #Variables 
+x0 = [3,2] #Vector inicial 
+tol = 10**-1 #Tolerancia 
+iterMax = 1000 #Iteraciones maximas
 
 res = newton_raphson(x0, f, x, tol, iterMax)
 
